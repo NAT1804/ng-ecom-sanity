28607048a3bf054c2c360b772585ab1a70fcb9aa
NAT1804 <167729136+Sotatek-TuanNguyen14@users.noreply.github.com>
2024-06-10 17:21:33 +0700

    feat: add robots.txt file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,36 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { Meta, provideClientHydration } from '@angular/platform-browser';
 import { vi_VN, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import vi from '@angular/common/locales/vi';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { environment } from '@environments/environment';
 
 registerLocaleData(vi);
+
+export function initIndexing(meta: Meta): () => Promise<void> {
+  return async () => {
+    const isProduction = environment.production === true;
+
+    meta.removeTag('name="robots"');
+    meta.addTag({
+      name: 'robots',
+      content: isProduction ? 'index, follow' : 'noindex, nofollow',
+    });
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +40,12 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(FormsModule),
     importProvidersFrom(HttpClientModule),
     provideAnimations(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initIndexing,
+      multi: true,
+      deps: [Meta],
+    },
   ],
 };
